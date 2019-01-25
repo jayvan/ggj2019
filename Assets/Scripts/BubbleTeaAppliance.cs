c4413fe2bf125be5b87029cd85dc8a487c5babdd
andrewstyll <andrewstyll@gmail.com>
2019-01-25 18:20:17 -0500

    added bubble tea landing area

---
+++ Assets/Scripts/BubbleTeaAppliance.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Transactions;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BubbleTeaAppliance : MonoBehaviour {
+
+    [SerializeField] private Button bubbleTeaMachine;
+    [SerializeField] private BubbleTeaArea teaArea;
+
+    void Start() {
+      this.bubbleTeaMachine.onClick.AddListener(StartTea);
+      bool foo = this.teaArea.IsFull;
+      this.teaArea.AddTea();
+
+    }
+
+    void Update() {
+
+    }
+
+  void StartTea() {
+    // check
+  }
+}
---
+++ Assets/Scripts/BubbleTeaArea.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BubbleTeaArea : MonoBehaviour
+{
+
+    private const int MAX_TEA = 3;
+    private int activeTeaCount = 0;
+
+    // Start is called before the first frame update
+    private List<GameObject> bubbleTeaQ;
+    [SerializeField] private GameObject bubbleTea;
+
+    void Start()
+    {
+        bubbleTeaQ = new List<GameObject>();
+        while(bubbleTeaQ.Count < MAX_TEA)
+        {
+            GameObject newTea = (GameObject)Instantiate(bubbleTea, gameObject.transform, false);
+            newTea.SetActive(false);
+            bubbleTeaQ.Add(newTea);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public bool IsFull
+    {
+        get
+        {   
+            return bubbleTeaQ.Count == MAX_TEA;
+        }
+    }
+
+    public void AddTea() 
+    {
+        if(activeTeaCount < MAX_TEA)
+        {
+            bubbleTeaQ[activeTeaCount++].SetActive(true);
+        }
+        else
+        {
+            throw new System.Exception("Too many tea's");
+        }
+         
+    }
+
+    public void RemoveTea()
+    {
+        if(activeTeaCount > 0)
+        {
+            bubbleTeaQ[--activeTeaCount].SetActive(false);
+        }
+    }
+}
