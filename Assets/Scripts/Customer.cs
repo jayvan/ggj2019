43e8630af154eb1894cf664781a2e2b5eca32253
andrewstyll <andrewstyll@gmail.com>
2019-01-25 19:37:21 -0500

    Added customer

--- Assets/Scripts/Customer.cs
+++ Assets/Scripts/Customer.cs
@@ -1,24 +1,44 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Customer : MonoBehaviour
 {
+    [SerializeField] private Slider patienceSlider;
+
+    private const float PATIENCE_MIN = 10.0f;
+    private const float PATIENCE_MAX = 15.0f;
+    private float patience;
+    private float timeRemaining;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        patience = Random.Range(PATIENCE_MIN, PATIENCE_MAX);
+        timeRemaining = patience;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (timeRemaining > 0) { 
+            timeRemaining -= Time.deltaTime;
+            patienceSlider.value = (patience - timeRemaining) / patience;
+        }
+        else
+        {
+            DestroyCustomer();
+        }
     }
 
-    void Serve()
+    private void DestroyCustomer()
     {
+        Destroy(this);
+    }
 
+    public void Serve()
+    {
+        DestroyCustomer();
     }
 }
